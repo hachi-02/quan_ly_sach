556faf6cb85c8a8af64e59d3e24ed630903f0184
HuuThang04 <thangphan300724@gmail.com>
2024-07-17 16:43:23 +0700

    moi sua

---
+++ app/src/main/java/dangnhap/welcome.java
@@ -0,0 +1,37 @@
+package dangnhap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.duanmau.R;
+
+public class welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.giaodien1);
+
+        CountDownTimer time=new CountDownTimer(3000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(welcome.this, dang_nhap.class);
+                startActivity(intent);
+                finish();
+            }
+        }.start();
+    }
+}
