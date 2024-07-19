b213c29d00fdf07d8640a97a7838dc7449a55537
HuuThang04 <thangphan300724@gmail.com>
2024-07-19 12:09:26 +0700

    fix đống bug, hoàn thành giao diện màn hình chính

--- app/src/main/java/ds_sanpham/SanPhamActivity.java
+++ app/src/main/java/ds_sanpham/SanPhamActivity.java
@@ -3,20 +3,28 @@ package ds_sanpham;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.duanmau.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import DAO.SanPhamDAO;
 import Model.SanPham;
@@ -28,22 +36,34 @@ public class SanPhamActivity extends AppCompatActivity {
     ArrayList<SanPham> ds;
     SanPhamAdapter adapter;
     FloatingActionButton fabutton;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_san_pham);
         rcv = findViewById(R.id.rcv);
-        bt_account = findViewById(R.id.bt_account);
+//        bt_account = findViewById(R.id.bt_account);
         fabutton = findViewById(R.id.floatactionbutton);
+        toolbar = findViewById(R.id.toolBar);
+        navigationView = findViewById(R.id.navigationView);
+        drawerLayout = findViewById(R.id.drawerLayout);
 
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(null);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+        ActionBar actionBar = getSupportActionBar();
 
         ds = new ArrayList<SanPham>();
         spd = new SanPhamDAO(SanPhamActivity.this);
         ds = spd.xemSP();
 //        sp.themSanPham(new SanPham("samsung", "truyện kiều", 5, 3000));
 //        sp.themSanPham(new SanPham("samsung", "truyện kiều", 5, 3000));
-      dulieu();
+        dulieu();
         adapter = new SanPhamAdapter(SanPhamActivity.this, ds);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
         rcv.setLayoutManager(layoutManager);
@@ -58,25 +78,32 @@ public class SanPhamActivity extends AppCompatActivity {
         });
     }
 
-    public void dulieu()
-    {
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+        {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+            return super.onOptionsItemSelected(item);
+    }
+
+    public void dulieu() {
         ds = spd.xemSP();
-        SanPhamAdapter adapter =new SanPhamAdapter(SanPhamActivity.this,ds);
+        SanPhamAdapter adapter = new SanPhamAdapter(SanPhamActivity.this, ds);
         LinearLayoutManager linear = new LinearLayoutManager(SanPhamActivity.this);
         rcv.setLayoutManager(linear);
         rcv.setAdapter(adapter);
     }
 
-    public void themSanPham()
-    {
+    public void themSanPham() {
         AlertDialog.Builder builder = new AlertDialog.Builder(SanPhamActivity.this);
         LayoutInflater inf = getLayoutInflater();
         View v = inf.inflate(R.layout.dialogthemsanpham, null);
         builder.setView(v);
-        EditText et_ten=v.findViewById(R.id.ten);
-        EditText et_theloai=v.findViewById(R.id.theloai);
-        EditText et_soluong=v.findViewById(R.id.soluong);
-        EditText et_gia=v.findViewById(R.id.giaban);
+        EditText et_ten = v.findViewById(R.id.ten);
+        EditText et_theloai = v.findViewById(R.id.theloai);
+        EditText et_soluong = v.findViewById(R.id.soluong);
+        EditText et_gia = v.findViewById(R.id.giaban);
 
         builder.setNegativeButton("no", new DialogInterface.OnClickListener() {
             @Override
@@ -87,45 +114,46 @@ public class SanPhamActivity extends AppCompatActivity {
         builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                String ten=et_ten.getText().toString();
-                String theloai=et_theloai.getText().toString();
-                int soluong=Integer.parseInt(et_soluong.getText().toString());
-                int dongia=Integer.parseInt(et_gia.getText().toString());
-                SanPham sp=new SanPham(ten,theloai,soluong,dongia);
+                String ten = et_ten.getText().toString();
+                String theloai = et_theloai.getText().toString();
+                int soluong = Integer.parseInt(et_soluong.getText().toString());
+                int dongia = Integer.parseInt(et_gia.getText().toString());
+                SanPham sp = new SanPham(ten, theloai, soluong, dongia);
                 spd.themSanPham(sp);
                 dulieu();
             }
         });
 
-        AlertDialog dialog=builder.create();
+        AlertDialog dialog = builder.create();
         dialog.show();
 
     }
-// xoa san pham
-//    public void xoaSanPham(int masp, int position) {
-//        AlertDialog.Builder builder = new AlertDialog.Builder(SanPhamActivity.this);
-//        builder.setTitle("thong bao");
-//        builder.setMessage("ban co muon xoa san pham nay khong");
-//        builder.setCancelable(false);
-//        builder.setNegativeButton("no", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                dialog.cancel();
-//            }
-//        });
-//        builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//
-//                spd.xoaSanPham(masp);
-//                adapter.remove(position);
-//            }
-//        });
-//
-//
-//        AlertDialog dialog = builder.create();
-//        dialog.show();
-//    }
+
+    // xoa san pham
+    public void xoaSanPham(int masp, int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SanPhamActivity.this);
+        builder.setTitle("thong bao");
+        builder.setMessage("ban co muon xoa san pham nay khong");
+        builder.setCancelable(false);
+        builder.setNegativeButton("no", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                spd.xoaSanPham(masp);
+                adapter.remove(position);
+            }
+        });
+
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 
     public void suaSanPham(SanPham sanPham, int position) {
         AlertDialog.Builder builder = new AlertDialog.Builder(SanPhamActivity.this);
